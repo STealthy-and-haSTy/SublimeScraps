ca619a4fe6f3750cfe772a001c5614c62ef814b0
Terence Martin <github@nurdz.com>
2018-07-17 21:05:56 -0700

    Include log_toggler.py
    This includes a simple plugin that allows you to easily toggle the state
    of the various logging mechanisms in Sublime on and off using a key
    binding, command palette entry or menu item.
    
    This also serves as a simple demonstration of using the `is_checked` and
    `description` methods of the command classes to customize how a command
    appears in the menu.
    
    As there is no API for determining the current state of the logging
    flags, the command tracks the current state itself, which means that it
    will get out of sync with reality if you use some external mechanism to
    toggle the logging.
    
    This will clear itself up the next time you invoke the command (although
    it may appear to do nothing), but on the whole this is probably not that
    important since you likely already know what you're doing in that case.

---
+++ plugins/log_toggler.py
@@ -0,0 +1,69 @@
+import sublime
+import sublime_plugin
+
+# This plugin provides a command that you can use to toggle the state of the
+# three internal Sublime logging commands on and off. It takes as an argument
+# the type of logging to toggle, which can be one of "log_commands",
+# "log_input" or "log_result_regex".
+#
+# Note that the plugin assumes that you're only using these commands to change
+# the state of the logging; since there is no API for getting the current state
+# for the log toggles, the command here can't detect if you've changed the
+# state of the logging via some external mechanism.
+#
+# This could be used in a key binding as well as in a sublime-commands or
+# sublime-menu file.
+#
+# In a sublime-menu, the command will display as checked when logging for that
+# type of log is enabled if  you include the "checkbox" attribute to the menu
+# entry. The command also has a default description which displays what command
+# it will execute when it's selected.
+#
+# As an example, you can add the following to a file named Context.sublime-menu
+# in your User package to include the commands in the context menu.
+#
+# [
+    # { "caption": "-", "id": "end" },
+    # {
+    #     "command": "toggle_sublime_logging",
+    #     "checkbox": true,
+    #     "caption": "Log Commands",
+    #     "args": {"log_type": "log_commands"},
+    # },
+    # {
+    #     "command": "toggle_sublime_logging",
+    #     "checkbox": true,
+    #     "caption": "Log Input",
+    #     "args": {"log_type": "log_input"},
+    # },
+    # {
+    #     "command": "toggle_sublime_logging",
+    #     "checkbox": true,
+    #     "caption": "Log Result Regex",
+    #     "args": {"log_type": "log_result_regex"},
+    # },
+# ]
+
+
+_log_state = {
+    "log_commands": False,
+    "log_input": False,
+    "log_result_regex": False
+}
+
+
+class ToggleSublimeLoggingCommand(sublime_plugin.ApplicationCommand):
+    """
+    Toggle the state of sublime's internal logging to the console based on the
+    log_type parameter, which should be one of the three sublime module API
+    endpoints, "log_command", "log_inoput" or "log_result_regex".
+    """
+    def run(self, log_type):
+        _log_state[log_type] = not _log_state[log_type]
+        getattr(sublime, log_type)(_log_state[log_type])
+
+    def description(self, log_type):
+        return "sublime.{0}({1})".format(log_type, not _log_state[log_type])
+
+    def is_checked(self, log_type):
+        return _log_state[log_type]
