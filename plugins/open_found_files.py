728f248482009da6acd6d854d3556736576e489d
Terence Martin <github@nurdz.com>
2019-12-21 19:59:21 -0800

    Clean up the open_all_found_files command
    Minor cosmetic changes to the code for this command have been done,
    mostly changing code style. In addition we now also disable the command
    outside of Find in Files output so that the command can be included in
    the command palette without constantly being available.
    
    The scope used in `find_by_selector` now technically has a redundant
    suffix since the command will only invoke in a file of the proper type,
    but that has been left in place here.

--- plugins/open_found_files.py
+++ plugins/open_found_files.py
@@ -6,12 +6,31 @@ import sublime_plugin
 
 # This adds a new command named "open_all_found_files" which can be bound to a
 # key, added to the command palette, etc. When you invoke the command while
-# inside of the results of a "Find in Files" operation it will locate all of the
-# filenames where matches were found and open them.
+# inside of the results of a "Find in Files" operation it will locate all of
+# the filenames where matches were found and open them.
 #
 # The command takes an optional parameter "new_window" which you can pass and
 # set to true if you want the results to open in a new window; otherwise they
 # open in the current window.
+#
+# To add this to the command palette, create a sublime-commands file that
+# contains the following in your User package, or add the commands to an
+# existing file if you have one; use 'Preferences > Browse Packages' to find
+# the location if you don't know where it is.
+#
+# [
+#     {
+#         "caption": "Find in Files: Open Matching Files",
+#         "command": "open_all_found_files"
+#     },
+#     {
+#         "caption": "Find in Files: Open Matching Files in New Window",
+#         "command": "open_all_found_files",
+#         "args": {
+#             "new_window": true
+#         }
+#     },
+# ]
 
 class OpenAllFoundFilesCommand(sublime_plugin.TextCommand):
     """
@@ -20,17 +39,20 @@ class OpenAllFoundFilesCommand(sublime_plugin.TextCommand):
     """
     def run(self, edit, new_window=False):
         # Collect all found filenames
-        positions = self.view.find_by_selector ("entity.name.filename.find-in-files")
+        positions = self.view.find_by_selector("entity.name.filename.find-in-files")
         if len(positions) > 0:
             # Set up the window to open the files in
             if new_window:
-                sublime.run_command ("new_window")
-                window = sublime.active_window ()
+                sublime.run_command("new_window")
+                window = sublime.active_window()
             else:
-                window = self.view.window ()
+                window = self.view.window()
 
             # Open each file in the new window
             for position in positions:
-                window.run_command ('open_file', {'file': self.view.substr (position)})
+                window.run_command('open_file', {'file': self.view.substr (position)})
         else:
-            self.view.window ().status_message ("No find results")
+            self.view.window().status_message("No find results")
+
+    def is_enabled(self):
+        return self.view.match_selector(0, "text.find-in-files")
